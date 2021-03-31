e436b87cb72c21d4657c22812fb9088c54a860d0
unknown <muhamed.webdevelopment@gmail.com>
2021-04-01 01:35:27 +0200

    ex04

--- ex04/src/App.js
+++ ex04/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+class App extends Component {
+  setLocalStorage() {   
+    localStorage.setItem("Arena", "Selection Month");
+     }
+  getLocalStorage() {
+  const myLocalStorageData = localStorage.getItem("Arena");   
+  return myLocalStorageData;
+  }   
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.setLocalStorage}>Set Local Storage</button>
+        <button onClick={this.getLocalStorage}>Get Local Storage</button>
+     </div>
+    );
+  }
 }
-
-export default App;
+export default App
